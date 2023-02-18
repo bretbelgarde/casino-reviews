ba93c71305275672d4131fbf0a0217ede9811b78
Bret Belgarde <bretbelgarde@gmail.com>
2023-02-18 14:03:46 -0500

    add shortcode and shortcode stub

--- includes/class-casino-reviews.php
+++ includes/class-casino-reviews.php
@@ -4,13 +4,33 @@ class CasinoReviews {
     /* Static propert for Singleton Instace */
     static $instance = false;
 
+    /** 
+     * Constructor
+     * 
+     * @return void
+     */
     private function __construct() {
-        
+       add_shortcode('crdisplay', [$this, 'cr_display']);
     }
 
+    /**
+     * Returns existing instance or creates new one
+     * 
+     * @return CasinoReviews
+     */
     public static function getInstance() {
         if (!self::$instance) self::$instance = new self;
         
         return self::$instance;
+    }
+
+    /**
+     * cr_display shortcode
+     * 
+     * @return string
+     * */
+    public function cr_display($attr) {
+        $key = sanitize_key($attr['key']);
+        return "KEY = {$key}";
     }
 }
