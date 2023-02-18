a279962d3c2471d9ef09e8c4e076f4482c2cb54f
Bret Belgarde <bretbelgarde@gmail.com>
2023-02-18 13:19:12 -0500

    Add class loader

---
+++ casino-reviews.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Casino Reviews
+ * 
+ * @author Bret Belgarde
+ * @version 0.0.1
+ * 
+ * @wordpress-plugin
+ * Plugin Name: Casino Reviews
+ * Description: Casino Review Plugin
+ * Version: 0.0.1
+ * Author: Bret Belgarde
+ */
+
+if (!defined('ABSPATH')) exit;
+
+//Plugin Constants
+define('CR_PLUGIN_FILE', __FILE__);
+define('CR_PLUGIN_DIR',  plugin_dir_path(CR_PLUGIN_FILE));
+
+// Casino Review Data Endpoint
+define('CASINOREVIEW_ENDPOINT_URL', 'http://mock.test/get');
+
+// Class Loader
+if (!class_exists('CasinoReviews')) {
+    require_once CR_PLUGIN_DIR . 'includes/class-casino-reviews.php';
+}
+
+$casino_reviews = CasinoReviews::getInstance();
